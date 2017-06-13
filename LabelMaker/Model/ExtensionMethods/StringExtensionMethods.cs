c77ae728974af79289185c0afb6d00f1640e49b2
mgove131 <mgove131@gmail.com>
2017-06-12 22:04:59 -0400

    ExtensionMethods

---
+++ LabelMaker/Model/ExtensionMethods/StringExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace LabelMaker.Model.ExtensionMethods
+{
+    public static class StringExtensionMethods
+    {
+        public static void SafeDelete(this string path)
+        {
+            try
+            {
+                File.Delete(path);
+            }
+            catch { }
+        }
+    }
+}
